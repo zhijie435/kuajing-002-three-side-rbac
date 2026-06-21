2549269884612170dc215d9b701199bbe127c462
zhijie435 <wushanxi@gmail.com>
2026-06-22 02:10:37 +0800

    .3999348844658896:a81d1b0a29509bcf396f2742928a0d6f_6a382616b1713b565ef3defc.6a38261fb1713b565ef3defe.6a38261fa95692f16ad36393:Trae CN.T(2026/6/22 01:57:51)

---
+++ server/tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+define('RBAC_TESTING', true);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../core/Controller.php';
+require_once __DIR__ . '/../controllers/RoleController.php';
+require_once __DIR__ . '/../controllers/MenuController.php';
+require_once __DIR__ . '/../controllers/PermissionController.php';
